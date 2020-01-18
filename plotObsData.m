67959ee0b4c63a8c5b0de98091b25da6e11d2561
arnauochoa <arnau.ochoa@gmail.com>
2020-01-18 21:57:33 +0100

    wls implemented. also some modifications and additions on plottings have been done

--- plotObsData.m
+++ plotObsData.m
@@ -6,7 +6,8 @@
 % ---------------------------------------------------------------------------------------
 
 %% HEADER
-close all; clc;
+close all; 
+clc;
 clearvars -except ObsData NavData
 addpath(genpath('Library'));
 set(groot,'defaultLineLineWidth',0.8)
@@ -19,26 +20,26 @@ fL1     =   1.57542e9;  %   [Hz]    Frequency of L1 band
 mkSize  =   12;         %    []     Marker size for plots
 
 %% FILE LOADING
-dataFileName = 'Data/Structs/multipath.mat';
+dataFileName = 'Data/Structs/static.mat';
 load(dataFileName);
 
 %% DATA EXTRACTION
 % Extraction of data into matrices
 nEpoch_max = length(ObsData.DATA); % For all epochs -> length(ObsData.DATA)
-[mEpoch, Nb_Epoch, vNb_Sat, Total_Nb_Sat, mTracked, mC1, mL1, mD1, mS1] = ...
-    ExtractData_O(ObsData.DATA, nEpoch_max);
+[~, nEpoch, vNumSat, ~, mTracked, mC1, mL1, mD1, mS1] = ExtractData_O(ObsData.DATA, nEpoch_max);
 
-[Iono_a, Iono_b, Ephem] = ExtractData_N(NavData.HEADER, NavData.DATA);
+%% COMPUTATIONS
+
 
 %% PLOTS
 % Variables used on plots
-tAxis   =   1:Nb_Epoch;
+tAxis   =   1:nEpoch;
 mColors =   jet(nSats); % Create set of different colors for plotting. 1 sat - 1 color. 
 
 % Number of tracked satellites over time
 figure;
-plot(tAxis, vNb_Sat);
-set(gca,'YTick',(min(vNb_Sat):1:max(vNb_Sat))); % Specifying Y axis steps of 1
+plot(tAxis, vNumSat);
+set(gca,'YTick',(min(vNumSat):1:max(vNumSat))); % Specifying Y axis steps of 1
 title('Number of tracked satellites over time');
 xlabel('Epoch'); ylabel('Number of tracked satellites');
 
@@ -52,7 +53,8 @@ xlabel('Epoch'); ylabel('Satellite');
 
 % Pseudoranges of satellites in view
 mC1Any  =   mC1(:, any(mC1));   % Keeping columns of satellites from which there's C1
-satsC1  =   find(any(mC1));     % Finding numbers of satellites from which there's C1
+[~, col]  =   find(mC1);          
+satsC1  =   unique(col);          % Finding ids of the satellites from which there's S1
 
 mC1Any(mC1Any==0) = nan;        % Changing values 0 for NaN so these aren't plotted
 
@@ -62,13 +64,14 @@ for sat = 1:length(satsC1)
     hold on
 end
 hold off
-legend(cellstr(num2str(satsC1')),'Location','bestoutside');
+legend(cellstr(num2str(satsC1)),'Location','bestoutside');
 title('Evolution of measured pseudoranges over time');
 xlabel('Epoch'); ylabel('Pseudorange [m]');
 
 % CNo of satellites in view
 mS1Any  =   mS1(:, any(mS1));   % Keeping columns of satellites from which there's S1
-satsS1  =   find(mS1(1,:));     % Finding numbers of satellites from which there's S1
+[~, col]=   find(mS1);          
+satsS1  =   unique(col);          % Finding ids of the satellites from which there's S1
 
 mS1Any(mS1Any==0) = nan;        % Changing values 0 for NaN so these aren't plotted
 
@@ -78,7 +81,7 @@ for sat = 1:length(satsS1)
     hold on
 end
 hold off
-legend(cellstr(num2str(satsS1')),'Location','bestoutside');
+legend(cellstr(num2str(satsS1)),'Location','bestoutside');
 title('Evolution of measured C/No over time');
 xlabel('Epoch'); ylabel('C/No [dB-Hz]');
 
@@ -86,7 +89,8 @@ xlabel('Epoch'); ylabel('C/No [dB-Hz]');
 mL1m        =   mL1 .* c/fL1;         % Transformation from cycles to meters
 mCMC1       =   mC1 - mL1;            % CMC computation
 mCMC1Any    =   mCMC1(:, any(mCMC1)); % Finding numbers of satellites from which there's CMC
-satsCMC1    =   find(mCMC1(1,:));     % Finding numbers of satellites from which there's S1
+[~, col]      =   find(mCMC1);          
+satsCMC1    =   unique(col);          % Finding ids of the satellites from which there's S1
 
 mCMC1Res    =   mCMC1Any - mean(mCMC1Any, 2); % Subtract the average to get rid of the ambiguity
 
@@ -98,7 +102,7 @@ for sat = 1:length(satsCMC1)
     hold on
 end
 hold off
-legend(cellstr(num2str(satsCMC1')),'Location','bestoutside');
+legend(cellstr(num2str(satsCMC1)),'Location','bestoutside');
 title('Evolution of measured CMC - E(CMC) over time');
 xlabel('Epoch'); ylabel('CMC [m]');
 
